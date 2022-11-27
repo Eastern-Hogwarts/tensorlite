29d7bdf552690efdd524a9028c09b438ed8858be
lygztq <lygztq@gmail.com>
2022-11-27 17:28:43 +0800

    rename fp16

--- include/tensorlite/float16.h
+++ include/tensorlite/float16.h
@@ -5,21 +5,20 @@
 #ifndef TENSORLITE_FP16_H_
 #define TENSORLITE_FP16_H_
 
-#include <fp16.h>
+#include "fp16.h"
 
 namespace tl {
 
 struct Float16 {
   uint16_t bits;
 
-  Half() = default;
-  Half(float f)
-    : bits(fp16_ieee_from_fp32_value(f)) {}
+  Float16() = default;
+  Float16(float f) : bits(fp16_ieee_from_fp32_value(f)) {}
   operator float() const { return fp16_ieee_to_fp32_value(bits); }
 
-#define DEFINE_BINARY_OP(op) \
-  Half operator op(Half other) const { \
-    return static_cast<float>(*this) op static_cast<float>(other); \
+#define DEFINE_BINARY_OP(op)                                                   \
+  Float16 operator op(Float16 other) const {                                   \
+    return static_cast<float>(*this) op static_cast<float>(other);             \
   }
 
   DEFINE_BINARY_OP(+)
@@ -27,8 +26,8 @@ struct Float16 {
   DEFINE_BINARY_OP(*)
   DEFINE_BINARY_OP(/)
 
-  static Half FromHex(uint16_t b) {
-    Half h;
+  static Float16 FromHex(uint16_t b) {
+    Float16 h;
     h.bits = b;
     return h;
   }
@@ -37,4 +36,4 @@ struct Float16 {
 using fp16_t = Float16;
 
 } // namespace tl
-#endif  // TENSORLITE_FP16_H_
+#endif // TENSORLITE_FP16_H_
